c7cb994dbf858e90061caa7055726786be701ead
paul guichon <paul.guichon@iedparis8.net>
2015-03-31 15:08:53 +0200

    migration django

---
+++ duck_theme_ied/xadmin_plugins/topnav.py
@@ -0,0 +1,29 @@
+
+from django.template import loader
+from django.utils.text import capfirst
+from django.core.urlresolvers import reverse, NoReverseMatch
+from django.utils.translation import ugettext as _
+
+from xadmin.sites import site
+from xadmin.filters import SEARCH_VAR
+from xadmin.views import BaseAdminPlugin, CommAdminView
+
+
+class IEDPlugin(BaseAdminPlugin):
+    urls = [
+        {'name': 'Wiki', 'url': '/wiki'},
+        # {'name': 'Forum', 'url': '/forum'},
+        {'name': 'Support', 'url': '/support'},
+    ]
+
+    def get_context(self, context):
+        return context
+
+    # Block Views
+    def block_top_navbar(self, context, nodes):
+        urls = self.urls
+        if self.request.user.is_superuser and len(urls) == 3:
+            urls.append({'name': 'Admin', 'url':'/admin'})
+        nodes.append(
+            loader.render_to_string('xadmin_plugins/comm.top.lien.html', {'urls': urls}))
+
